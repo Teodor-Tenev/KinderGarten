7e6f452fc12eb6cd7f9941ae46c2ab96f8e38298
TeodorTenev <teodor.t.tenev@gmail.com>
2020-04-12 17:44:02 +0300

    Adding Kindergarden project

---
+++ KinderGarden/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KinderGarden
+{
+    public class Child
+    {
+        public Child(string name, int iD, double points, int wish)
+        {
+            Name = name;
+            ID = iD;
+            Points = points;
+            Wish = wish;
+        }
+
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public double Points { get; set; }
+        public int Wish { get; set; }
+
+    }
+}
---
+++ KinderGarden/KinderGarden.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KinderGarden
+{
+    public class KinderGarden
+    {
+        public KinderGarden(string uRL, double distanceFromHome, int approxTimeInMinutes)
+        {
+            DistanceFromHome = distanceFromHome;
+            ApproxTimeInMinutes = approxTimeInMinutes;
+            URL = uRL;
+        }
+
+        public double DistanceFromHome { get; set; }
+        public int ApproxTimeInMinutes { get; set; }
+        public string URL { get; set; }
+        public string Name { get; set; }
+        public int FreeSlots { get; set; }
+        public List<Child> Children { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"\"{Name}\" има свободни {FreeSlots} места.");
+            for (int i = 1; i <= Children.Count; i++)
+            {
+                var child = Children[i-1];
+                Console.WriteLine($"{i}. {child.Name} {child.ID} {child.Points} {child.Wish}");
+            }
+            Console.WriteLine(new String('=', 12));
+
+        }
+    }
+}
